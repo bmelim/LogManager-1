7f635094f2decbc8be2ffe0247ee4478b48dcf12
nomis51 <nomis51@users.noreply.github.com>
2018-05-08 14:30:22 -0400

    init commit

---
+++ LogManager/AppController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace LogManager {
+    public class AppController {
+        #region Singleton
+        private static AppController Instance { get; set; }
+        public static AppController GetInstance() {
+            if (Instance == null) {
+                Instance = new AppController("LogManagerConfig.json");
+            }
+
+            return Instance;
+        }
+        #endregion
+
+        private string ConfigPath { get; set; }
+        private Dictionary<string, object> Config { get; set; }
+
+        private AppController(string configPath) {
+            this.ConfigPath = configPath;
+
+            ReadConfig();
+
+            if (!this.Config.ContainsKey("directories")) {
+                this.Config = new Dictionary<string, object>();
+            }
+        }
+
+        private void ReadConfig() {
+            this.Config = JsonConvert.DeserializeObject<Dictionary<string, object>>(File.ReadAllText(this.ConfigPath));
+        }
+
+        private void WriteConfig() {
+            File.WriteAllText(this.ConfigPath, JsonConvert.SerializeObject(this.Config));
+        }
+
+        public bool AddNewDirectory(string path) {
+            ReadConfig();
+
+            var entry = new Dictionary<string, string>();
+            entry.Add("path", path);
+            ((JArray)this.Config["directories"]).Add(JToken.FromObject(entry));
+
+            WriteConfig();
+
+            return true;
+        }
+
+        public List<Dictionary<string, string>> GetDirectories() {
+            ReadConfig();
+
+            List<Dictionary<string, string>> dirs = JsonConvert.DeserializeObject<List<Dictionary<string, string>>>(JsonConvert.SerializeObject(this.Config["directories"]));
+
+            return dirs;
+        }
+    }
+}
---
+++ LogManager/LogDirectories.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LogManager {
+    /// <summary>
+    /// Logique d'interaction pour LogDirectories.xaml
+    /// </summary>
+    public partial class LogDirectories : Window {
+        public LogDirectories() {
+            InitializeComponent();
+            RefreshLogDirList();
+        }
+
+        private void btnAddNewLogDir_Click(object sender, RoutedEventArgs e) {
+            AddNewLogDir();
+            RefreshLogDirList();
+        }
+
+        private void AddNewLogDir() {
+            using (var fbd = new System.Windows.Forms.FolderBrowserDialog()) {
+                System.Windows.Forms.DialogResult result = fbd.ShowDialog();
+
+                if (result == System.Windows.Forms.DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath)) {
+                    AppController.GetInstance().AddNewDirectory(fbd.SelectedPath);
+                }
+            }
+        }
+
+        private void btnRefreshLogDirList_Click(object sender, RoutedEventArgs e) {
+            RefreshLogDirList();
+        }
+
+        private void RefreshLogDirList() {
+            this.spLogDirs.Children.Clear();
+
+            var dirs = AppController.GetInstance().GetDirectories();
+
+            foreach (var d in dirs) {
+                DockPanel dockPanel = new DockPanel();
+
+                Label lblPathValue = new Label();
+                lblPathValue.Content = d["path"];
+
+                dockPanel.Children.Add(lblPathValue);
+
+                this.spLogDirs.Children.Add(dockPanel);
+            }
+        }
+    }
+}
